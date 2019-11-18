b8a59f49464dfbca7cce01e582881cd1e9270cb5
Hoang Nguyen <hoang@focal-labs.com>
2019-11-19 06:47:10 +0700

    init project

--- App.tsx
+++ App.tsx
@@ -1,49 +1,128 @@
 import React, { useEffect, useRef } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import SocketIOClient from "socket.io-client";
 import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Alert,
+  Button,
+  StatusBar
+} from "react-native";
+import SocketIOClient from "socket.io-client";
+const {
   RTCPeerConnection,
   RTCIceCandidate,
   RTCSessionDescription,
-  RTCView,
   MediaStream,
-  MediaStreamTrack,
-  mediaDevices,
-  registerGlobals
-} from "react-native-webrtc";
+  MediaStreamTrack
+} = Platform.OS === "web" ? window : require("react-native-webrtc");
+
+const notify = (title: string, message?: string) => {
+  if (Platform.OS === "web") {
+    alert(title);
+  } else {
+    Alert.alert(title);
+  }
+};
 
 export default function App() {
   let pc = useRef<RTCPeerConnection | null>(null);
   let socket = useRef<SocketIOClient.Socket | null>(null);
 
+  const makeOffer = () => {
+    if (pc.current) {
+      console.log("make offer");
+      pc.current.createOffer().then(offer => {
+        pc.current.setLocalDescription(offer).then(() => {
+          socket.current.emit("MAKE_OFFER", offer);
+        });
+      });
+    } else {
+      notify("Can not connect to Directory, please connect to the same wifi");
+    }
+  };
+
   useEffect(() => {
     pc.current = new RTCPeerConnection({
-      iceServers: [{ url: "stun:stun.services.mozilla.com" }]
+      iceServers: [{ url: "stun:stun.l.google.com:19302" }]
     });
 
-    socket.current = SocketIOClient("http://192.168.1.6:3000");
+    pc.current.onicecandidate = event => {
+      console.log("on iceeeee");
+      socket.current.emit("data", event.candidate);
+    };
 
-    socket.current.on("CLIENTS_LIST", clients => {
-      console.log("list clients: ", clients);
+    socket.current = SocketIOClient("http://192.168.48.27:3000");
+
+    socket.current.on("PEERS_LIST", peers => {
+      console.log("list peers: ", peers);
     });
 
-    pc.current.createOffer().then(offer => {
-      pc.current.setLocalDescription(offer).then(() => {
-        socket.current.emit("MAKE_OFFER", offer);
+    socket.current.on("DIRECTORY_BUSY", () => {
+      notify("Directory is currently in use");
+    });
+
+    socket.current.on("data", message => {
+      console.log("message", message);
+      if (message.sdp) {
+        if (message.type === "answer") {
+          const remoteDecription = new RTCSessionDescription(message);
+          pc.current
+            .setRemoteDescription(remoteDecription)
+            .catch(error => console.error(error));
+        }
+      } else if (message.candidate) {
+        const iceCandidate = new RTCIceCandidate(RTCIceCandidate);
+        pc.current.addIceCandidate(iceCandidate);
+        console.log("browser added ice candidate: ", iceCandidate);
+      }
+    });
+
+    // if is directory
+    if (Platform.OS === "android") {
+      socket.current.emit("SET_DIRECTORY", {
+        busy: false
       });
-    });
+      socket.current.on("data", message => {
+        // notify(JSON.stringify(message));
+        console.log("message", message);
+        if (message.sdp) {
+          if (message.type === "offer") {
+            console.log("answer offer");
+            const remoteDecription = new RTCSessionDescription(message);
+            pc.current
+              .setRemoteDescription(remoteDecription)
+              .then(() => {
+                pc.current.createAnswer().then(answer => {
+                  console.log("answer: ", answer);
+                  socket.current.emit("MAKE_ANSWER", answer);
+                });
+              })
+              .catch(error => console.error(error));
+          }
+        } else if (message.candidate) {
+          const iceCandidate = new RTCIceCandidate(RTCIceCandidate);
+          pc.current.addIceCandidate(iceCandidate);
+          console.log("android added candidate: ", iceCandidate);
+        }
+      });
+    }
 
     return () => {};
   }, []);
 
   return (
     <View style={styles.container}>
+      <StatusBar hidden={true} />
       <View
         style={{ width: "40%", height: "100%", backgroundColor: "green" }}
       ></View>
       <View
         style={{ width: "60%", height: "100%", backgroundColor: "blue" }}
       ></View>
+      <View style={{ position: "absolute", marginTop: 20 }}>
+        <Button title="CONTROL DIRECTORY" onPress={makeOffer} />
+      </View>
     </View>
   );
 }
@@ -52,7 +131,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
     flexDirection: "row"
   }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,57 @@
 const io = require("socket.io")(3000);
+const _ = require("lodash");
 
-let clients = [];
+let directory;
+
+let peers = [];
 
 io.on("connection", socket => {
   console.log("new socket client: ", socket.id);
   socket.on("MAKE_OFFER", offer => {
-    clients.push({
+    if (peers.length < 2 && directory && !directory.busy) {
+      peers.push({
+        socketId: socket.id,
+        description: offer
+      });
+      io.sockets.emit("PEERS_LIST", peers);
+      io.to(directory.socketId).emit("data", offer);
+    } else {
+      socket.emit("DIRECTORY_BUSY");
+    }
+  });
+
+  socket.on("MAKE_ANSWER", message => {
+    if (
+      socket.id === directory.socketId &&
+      message.sdp &&
+      message.type === "answer"
+    ) {
+      const controller = _.find(
+        peers,
+        peer => peer.socketId !== directory.socketId
+      );
+      if (controller) {
+        console.log("emit anser");
+        io.to(controller.socketId).emit("data", message);
+      }
+      return;
+    }
+  });
+
+  socket.on("data", message => {
+    console.log("socket on ldata");
+    const otherPeer = _.find(peers, peer => peer.socketId !== socketId);
+    if (otherPeer) {
+      io.to(otherPeer.socketId).emit("data", message);
+    }
+  });
+
+  socket.on("SET_DIRECTORY", directoryData => {
+    directory = {
       socketId: socket.id,
-      description: offer
-    });
-    console.log("clients: ", clients);
-    io.sockets.emit("CLIENTS_LIST", clients);
+      busy: directoryData.busy
+    };
+    peers[0] = directory;
+    console.log("set directory device: ", directory);
   });
 });
