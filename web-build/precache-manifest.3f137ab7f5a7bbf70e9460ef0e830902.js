b2981e463cf777e8451699e305945a3a3a77798f
Hoang Nguyen <hoang@focal-labs.com>
2019-11-19 06:52:29 +0700

    add web build

---
+++ web-build/precache-manifest.3f137ab7f5a7bbf70e9460ef0e830902.js
@@ -0,0 +1,100 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c551082509b1660c3e9",
+    "url": "/static/js/app.d23a96df.chunk.js"
+  },
+  {
+    "revision": "440be4b4ed4ec37d369a",
+    "url": "/static/js/runtime~app.82cccf13.js"
+  },
+  {
+    "revision": "c25d3414cf795ba89a4c",
+    "url": "/static/js/2.03ad3d28.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "f260500f3382eb73595a365daaab161f",
+    "url": "/manifest.json"
+  },
+  {
+    "url": "/apple/icons/icon_192x192.5cd221e1f00589d415133b1afdf680e8.png"
+  },
+  {
+    "url": "/apple/icons/icon_512x512.98bb6a9f670dc5a2d51c428f77539a44.png"
+  },
+  {
+    "revision": "289b25f8acc5248c9e842efe0631d016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "930a740d79cd4305f651c76f4771fb3f",
+    "url": "/static/js/app.d23a96df.chunk.js.gz"
+  },
+  {
+    "revision": "b460a9bb76b08d0e02560573c1220b3e",
+    "url": "/static/js/runtime~app.82cccf13.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "e6bb855fefbfc21692e79e96076c55e7",
+    "url": "/static/js/2.03ad3d28.chunk.js.gz"
+  }
+];
